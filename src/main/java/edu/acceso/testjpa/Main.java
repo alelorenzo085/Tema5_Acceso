2e8a3e3c604854109ba4043ea1dd0694dbde356f
Alejandro Lorenzo <alormel012@g.educaand.es>
2026-02-05 10:15:18 +0100

    Añadidas transacciones ordenar y agrupar con Metamodel

--- src/main/java/edu/acceso/testjpa/Main.java
+++ src/main/java/edu/acceso/testjpa/Main.java
@@ -21,30 +21,31 @@ import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 
 public class Main {
-        private static Logger logger = LoggerFactory.getLogger(Main.class);
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
 
-        ch.qos.logback.classic.Logger hibernateLogger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("org.hibernate");
+        ch.qos.logback.classic.Logger hibernateLogger = (ch.qos.logback.classic.Logger) LoggerFactory
+                .getLogger("org.hibernate");
         hibernateLogger.setLevel(Level.WARN);
 
         String bd = "jdbc:sqlite:centro.db";
-        
+
         Map<String, String> props = new HashMap<>();
-        //props.put("jakarta.persistence.jdbc.url", bd);
+        // props.put("jakarta.persistence.jdbc.url", bd);
 
         // Se crea un objeto para realizar transacciones
         int idx = JpaBackend.createEntityManagerFactory("InstitutoPersistente", props);
 
         // Transacción sin resultado para agregar un centro Castillo de Luna
         JpaBackend.transaction(idx, em -> {
-            Centro centro = new Centro(11004866, "IES Castillo de Luna",Titularidad.PUBLICA);
+            Centro centro = new Centro(11004866, "IES Castillo de Luna", Titularidad.PUBLICA);
             em.persist(centro);
         });
 
         // Transacción sin resultado para agregar un centro Lara
         JpaBackend.transaction(idx, em -> {
-            Centro centro = new Centro(11700603, "IES Lara del Rey",Titularidad.PUBLICA);
+            Centro centro = new Centro(11700603, "IES Lara del Rey", Titularidad.PUBLICA);
             em.persist(centro);
         });
 
@@ -63,15 +64,16 @@ public class Main {
         // Transacción sin resultado para agregar una lista de estudiantes
         JpaBackend.transaction(idx, em -> {
             Estudiante[] estudiantes = new Estudiante[] {
-                new Estudiante("Manolo", LocalDate.of(2000, 01, 01), castillo),
-                new Estudiante("Marisa", LocalDate.of(2004, 10, 12), castillo),
-                new Estudiante("Alberto", LocalDate.of(2003, 5, 22), castillo),
-                new Estudiante("Lucía", LocalDate.of(2002, 7, 2), lara),
-                new Estudiante("Paco", LocalDate.of(2004, 7, 21), null)
+                    new Estudiante("Manolo", LocalDate.of(2000, 01, 01), castillo),
+                    new Estudiante("Marisa", LocalDate.of(2004, 10, 12), castillo),
+                    new Estudiante("Alberto", LocalDate.of(2003, 5, 22), castillo),
+                    new Estudiante("Lucía", LocalDate.of(2002, 7, 2), lara),
+                    new Estudiante("Paco", LocalDate.of(2004, 7, 21), null)
             };
 
             // Por cada estudiante de la lista, se añade a la base de datos (acción persist)
-            for(Estudiante e: estudiantes) em.persist(e);
+            for (Estudiante e : estudiantes)
+                em.persist(e);
         });
 
         // Comprobar estudiantes
@@ -83,9 +85,11 @@ public class Main {
         System.out.printf("-- Estudiantes de '%s' --\n ", castillo.getNombre() + ":");
         estudiantes.forEach(System.out::println);
 
-        // Creamos una transacción nueva para obtener únicamente los nombres de los estudiantes
+        // Creamos una transacción nueva para obtener únicamente los nombres de los
+        // estudiantes
         JpaBackend.transaction(em -> {
-            // Se establece una consulta a la tabla de Estudiante donde obtenemos solo su nombre
+            // Se establece una consulta a la tabla de Estudiante donde obtenemos solo su
+            // nombre
             TypedQuery<String> query = em.createQuery("SELECT e.nombre FROM Estudiante e", String.class);
 
             // Y se establece una lista con los nombres obtenidos
@@ -97,8 +101,7 @@ public class Main {
         // Transacción con consulta JOIN para extraer todos los estudiantes con centro
         estudiantes = JpaBackend.transactionR(em -> {
             TypedQuery<Estudiante> ee = em.createQuery(
-                "SELECT e FROM Estudiante e JOIN e.centro c", Estudiante.class
-            );
+                    "SELECT e FROM Estudiante e JOIN e.centro c", Estudiante.class);
             return ee.getResultList();
         });
 
@@ -108,13 +111,12 @@ public class Main {
         // Transacción con consulta JOIN para extraer todos los estudiantes
         estudiantes = JpaBackend.transactionR(em -> {
             TypedQuery<Estudiante> ee = em.createQuery(
-                "SELECT e FROM Estudiante e LEFT JOIN e.centro c", Estudiante.class
-            );
+                    "SELECT e FROM Estudiante e LEFT JOIN e.centro c", Estudiante.class);
             return ee.getResultList();
         });
 
         System.out.println("--- Lista de todos los estudiantes ---");
-        for(Estudiante e: estudiantes) {
+        for (Estudiante e : estudiantes) {
             Centro centro = e.getCentro();
             System.out.printf("%s: %s.\n", e, centro != null ? centro.getNombre() : "***");
         }
@@ -145,10 +147,9 @@ public class Main {
             System.out.println("\n-- Lista de nombres e IDs de estudiantes con Criteria ---");
             CriteriaQuery<Tuple> criteriaT = cb.createTupleQuery();
             root = criteriaT.from(Estudiante.class);
-            criteriaT.select( cb.tuple(
-                root.get(Estudiante_.nombre).alias("nombre"),
-                root.get(Estudiante_.id).alias("id")
-            ));
+            criteriaT.select(cb.tuple(
+                    root.get(Estudiante_.nombre).alias("nombre"),
+                    root.get(Estudiante_.id).alias("id")));
 
             TypedQuery<Tuple> tqT = em.createQuery(criteriaT);
             tqT.getResultList().forEach(t -> {
@@ -160,16 +161,46 @@ public class Main {
             // LISTA DE ESTUDIANTES NO MATRICULADOS USANDO CONDICIONES WHERE
             CriteriaQuery<Estudiante> criteriaNM = cb.createQuery(Estudiante.class);
             Root<Estudiante> rootNM = criteriaNM.from(Estudiante.class);
-            criteria.select(rootNM);
+            criteriaNM.select(rootNM);
             // CONDICIÓN
-            criteria.where(cb.not(cb.isNull(rootNM.get(Estudiante_.CENTRO))));
+            criteriaNM.where(cb.not(cb.isNull(rootNM.get(Estudiante_.CENTRO))));
 
             System.out.println("\n-- Lista de estudiantes sin matrícula ---");
-            TypedQuery<Estudiante> tqNM = em.createQuery(criteriaNM);
-            tqNM.getResultList().forEach(System.out::println);
-            });
+            TypedQuery<Estudiante> te = em.createQuery(criteriaNM);
+            te.getResultList().forEach(System.out::println);
+        });
 
-        // Resetea el hashmap de valores y objetos y cierra objetos abiertos
-        JpaBackend.reset();
+        JpaBackend.transaction(em -> {
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+
+            CriteriaQuery<Estudiante> query = cb.createQuery(Estudiante.class);
+            Root<Estudiante> root = query.from(Estudiante.class);
+            query.select(root);
+            query.orderBy(cb.desc(root.get(Estudiante_.nacimiento)));
+
+            System.out.println("\n -- Lista de estudiantes ordenados por edad --");
+            TypedQuery<Estudiante> tq = em.createQuery(query);
+            tq.getResultList().forEach(System.out::println);
+        });
+
+        JpaBackend.transaction(em -> {
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+
+            CriteriaQuery<Tuple> query = cb.createQuery(Tuple.class);
+            Root<Estudiante> root = query.from(Estudiante.class);
+            query.select(cb.tuple(
+                root.get(Estudiante_.CENTRO).alias("centro"),
+                cb.count(root).alias("alias")
+            ));
+            query.groupBy(root.get(Estudiante_.CENTRO));
+
+            System.out.println("\n-- Lista de centros --");
+            TypedQuery<Tuple> tq = em.createQuery(query);
+            tq.getResultList().forEach(t -> {
+                Centro centro = t.get("centro", Centro.class);
+                Long cantidad = t.get("cantidad", Long.class);
+                System.out.printf("%s: %d alumnos.\n", centro, cantidad);
+            });
+        });
     }
 }
